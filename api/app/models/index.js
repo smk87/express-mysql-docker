52d4e2e22e41267e88514b6b9b9c28d9391b9d36
Saqlain Mahmud <sqlnmhmdkhanemon@gmail.com>
2020-11-25 16:21:02 +0600

    FIX: Sequalize orm connect issue.

--- api/app/models/index.js
+++ api/app/models/index.js
@@ -5,13 +5,28 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST,
     port: dbConfig.PORT,
     dialect: dbConfig.dialect,
-    operatorsAliases: false,
+    // operatorsAliases: false,
 
     pool: {
         max: dbConfig.pool.max,
         min: dbConfig.pool.min,
         acquire: dbConfig.pool.acquire,
         idle: dbConfig.pool.idle,
+    },
+    retry: {
+        match: [
+            /SequelizeConnectionError/,
+            /SequelizeConnectionRefusedError/,
+            /SequelizeHostNotFoundError/,
+            /SequelizeHostNotReachableError/,
+            /SequelizeInvalidConnectionError/,
+            /SequelizeConnectionTimedOutError/,
+        ],
+        name: 'query',
+        backoffBase: 100,
+        backoffExponent: 1.1,
+        timeout: 60000,
+        max: Infinity,
     },
 });
 
